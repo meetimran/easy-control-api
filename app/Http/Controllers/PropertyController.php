d5bef8851eccb0d0dc57653b614caefdc35e8763
Imran Ahmad <meetimrang@gmail.com>
2022-07-21 12:39:54 +0500

    Added API requests for 'Properties'

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Property;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class PropertyController extends Controller
 {
@@ -14,7 +15,8 @@ class PropertyController extends Controller
      */
     public function index()
     {
-        //
+        $res = Property::all();
+        return response($res, 200);
     }
 
     /**
@@ -35,7 +37,64 @@ class PropertyController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $this->validate($request, [
+            'isActive' => 'integer|nullable',
+            'property_id' => 'integer|nullable',
+            'CreatedBy' =>'nullable',
+            'LastUpdatedBy' =>'nullable',
+            'CreatedByName' =>'nullable',
+            'LastUpdatedByName' =>'nullable',
+            'SortKey' => 'integer|nullable',
+
+            'PropertyName' => 'nullable',
+            'PropertyPrivateKey' => 'nullable',
+            'Description' => 'nullable',
+            'BeneficiaryName' => 'nullable',
+            'RegisterationNo1' => 'nullable',
+            'RegisterationNo2' => 'nullable',
+            'RegisterationNo3' => 'nullable',
+            'RegisterationNo4' => 'nullable',
+            'RegisterationNo5' => 'nullable',
+            'Room' => 'nullable',
+            'PropertyGrade' => 'nullable',
+            'PropertyType' => 'nullable',
+            'Address' => 'nullable',
+            'LogoImgPath' => 'nullable',       
+
+            'OptimisticLockField' => 'integer|nullable',
+            'GCRecord' => 'integer|nullable'
+        ]);
+
+        // dd($validated);
+        $newProperty =new Property();
+        $newProperty->isActive = $validated['isActive'];
+        $newProperty->property_id = $validated['property_id'];
+        $newProperty->CreatedBy = $validated['CreatedBy'];
+        $newProperty->LastUpdatedBy = $validated['LastUpdatedBy'];
+        $newProperty->CreatedByName = $validated['CreatedByName'];
+        $newProperty->LastUpdatedByName = $validated['LastUpdatedByName'];
+        $newProperty->SortKey = $validated['SortKey'];
+
+        $newProperty->PropertyName = $validated['PropertyName'];
+        $newProperty->PropertyPrivateKey = $validated['PropertyPrivateKey'];
+        $newProperty->Description = $validated['Description'];
+        $newProperty->BeneficiaryName = $validated['BeneficiaryName'];
+        $newProperty->RegisterationNo1 = $validated['RegisterationNo1'];
+        $newProperty->RegisterationNo2 = $validated['RegisterationNo2'];
+        $newProperty->RegisterationNo3 = $validated['RegisterationNo3'];
+        $newProperty->RegisterationNo4 = $validated['RegisterationNo4'];
+        $newProperty->RegisterationNo5 = $validated['RegisterationNo5'];
+        $newProperty->Room = $validated['Room'];
+        $newProperty->PropertyGrade = $validated['PropertyGrade'];
+        $newProperty->PropertyType = $validated['PropertyType'];
+        $newProperty->Address = $validated['Address'];
+        $newProperty->LogoImgPath = $validated['LogoImgPath'];
+
+        $newProperty->OptimisticLockField = $validated['OptimisticLockField'];
+        $newProperty->GCRecord = $validated['GCRecord'];
+        $newProperty->save();
+
+        return response($newProperty->refresh(), 200);
     }
 
     /**
@@ -55,9 +114,64 @@ class PropertyController extends Controller
      * @param  \App\Models\Property  $property
      * @return \Illuminate\Http\Response
      */
-    public function edit(Property $property)
+    public function edit(Request $request, Property $property)
     {
-        //
+        $validated = $this->validate($request, [
+            'isActive' => 'integer|nullable',
+            'property_id' => 'integer|nullable',
+            'CreatedBy' =>'nullable',
+            'LastUpdatedBy' =>'nullable',
+            'CreatedByName' =>'nullable',
+            'LastUpdatedByName' =>'nullable',
+            'SortKey' => 'integer|nullable',
+
+            'PropertyName' => 'nullable',
+            'PropertyPrivateKey' => 'nullable',
+            'Description' => 'nullable',
+            'BeneficiaryName' => 'nullable',
+            'RegisterationNo1' => 'nullable',
+            'RegisterationNo2' => 'nullable',
+            'RegisterationNo3' => 'nullable',
+            'RegisterationNo4' => 'nullable',
+            'RegisterationNo5' => 'nullable',
+            'Room' => 'nullable',
+            'PropertyGrade' => 'nullable',
+            'PropertyType' => 'nullable',
+            'Address' => 'nullable',
+            'LogoImgPath' => 'nullable',       
+
+            'OptimisticLockField' => 'integer|nullable',
+            'GCRecord' => 'integer|nullable'
+        ]);
+
+        $property->isActive = $validated['isActive'];
+        $property->property_id = $validated['property_id'];
+        $property->CreatedBy = $validated['CreatedBy'];
+        $property->LastUpdatedBy = $validated['LastUpdatedBy'];
+        $property->CreatedByName = $validated['CreatedByName'];
+        $property->LastUpdatedByName = $validated['LastUpdatedByName'];
+        $property->SortKey = $validated['SortKey'];
+
+        $property->PropertyName = $validated['PropertyName'];
+        $property->PropertyPrivateKey = $validated['PropertyPrivateKey'];
+        $property->Description = $validated['Description'];
+        $property->BeneficiaryName = $validated['BeneficiaryName'];
+        $property->RegisterationNo1 = $validated['RegisterationNo1'];
+        $property->RegisterationNo2 = $validated['RegisterationNo2'];
+        $property->RegisterationNo3 = $validated['RegisterationNo3'];
+        $property->RegisterationNo4 = $validated['RegisterationNo4'];
+        $property->RegisterationNo5 = $validated['RegisterationNo5'];
+        $property->Room = $validated['Room'];
+        $property->PropertyGrade = $validated['PropertyGrade'];
+        $property->PropertyType = $validated['PropertyType'];
+        $property->Address = $validated['Address'];
+        $property->LogoImgPath = $validated['LogoImgPath'];
+
+        $property->OptimisticLockField = $validated['OptimisticLockField'];
+        $property->GCRecord = $validated['GCRecord'];
+        $property->save();
+
+        return response($property->refresh(), 200);
     }
 
     /**
@@ -80,6 +194,6 @@ class PropertyController extends Controller
      */
     public function destroy(Property $property)
     {
-        //
+        return response($property->delete(), 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PropertyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,15 +22,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::prefix('v1')->group(function(){
-    Route::controller(UserController::class)->group(function(){
-        Route::post("login",[UserController::class,'login']);
-        Route::post('/users','create');
 
+        //FOR USER LOGIN
+        Route::post("login",[UserController::class,'login']);
+
+        //FOR USER REGISTER
+        Route::post('/users',[UserController::class,'create']);
+
+        //AUTHENTICATED REQUESTS
         Route::group(['middleware' => 'auth:sanctum'], function(){
-            Route::get('/users','show');
-            Route::delete('/users/{id}','destroy');
-            Route::put('/users/{id}','update');
-            });
+
+            //for Users
+            Route::get('/users',[UserController::class,'show']);
+            Route::delete('/users/{id}',[UserController::class,'destroy']);
+            Route::put('/users/{id}',[UserController::class,'update']);
+
+
+            //for Properties
+            Route::post('/properties',[PropertyController::class,'store']);
+            Route::get('/properties',[PropertyController::class,'index']);
+            Route::delete('/properties/{property}',[PropertyController::class,'destroy']);
+            Route::put('/properties/{property}',[PropertyController::class,'edit']);
+
         });    
     });
     
